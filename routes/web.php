b199bc1454b1c6baf1fbdd9557eaca42634e58b2
SC-shunemuk <go.shun.ku@gmail.com>
2021-01-09 17:34:13 +0900

    任意パラメータ

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('hello/{msg}',function($msg){
+Route::get('hello/{msg?}',function($msg = 'no message.'){
 $html = <<<EOF
 <html>
 <head>
